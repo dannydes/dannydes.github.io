6b9affce5eeed00659c4b439d2ce07cabea597d5
Daniel <desiradaniel2007@hotmail.com>
2016-02-22 18:10:36 +0100

    more work on contact form

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,16 @@
 (function ( $ ) {
 	$( '#contact-form' ).submit(function () {
-		$.post( 'myurl', {
+		$.post( 'http://contact-dannyde.byethost32.com/index.php', {
 			name: $( '#name' ).val(),
 			email: $( '#email' ).val(),
-			message: $( '#message' ).val()
+			subject: $( '#subject' ).val(),
+			message: $( '#message' ).val(),
+			hidden: $( '#hidden' ).val()
 		}, function ( data ) {
 			if ( data === '1' ) {
 				$( '#success' ).modal( 'show' );
+			} else {
+				$( '#failure' ).model( 'show' );
 			}
 		});
 		return false;
